122ab53a5b5f3a8eeda25cd8c26be24656e1d18b
DerrickLapp <Derrick.Lapp@sdgku.edu>
2025-01-15 16:44:01 -0800

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,26 @@
+function sayHello(){
+    console.log("Hello");
+}
+
+function init(){
+    console.log("hello world!");   
+    sayHello();
+    sayHi("Derrick");azsa
+}
+window.onload = init;
+
+// Variable Scope
+let var1 = 1;
+function scope(){
+    let var1 = 2;
+}
+for(let i=0;i<scope[i].length;i++){
+    for(let i=0;i<scope[i].length;i++){
+    }
+}
+
+function sayHi(name){
+    console.log("hi "+ name);
+    
+}
+
